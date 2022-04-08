9574d95202e2e6dae196f9b32dea945e8b52f2b3
nick7255 <nikrass@cs.ihu.gr>
2022-04-08 21:48:44 +0300

    only add who wins and name of creator and done

--- src/main/java/com/example/dotsandlinesgame/gameController.java
+++ src/main/java/com/example/dotsandlinesgame/gameController.java
@@ -2,16 +2,20 @@ package com.example.dotsandlinesgame;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Font;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Objects;
-
 
 public class gameController{
     @FXML
@@ -25,9 +29,13 @@ public class gameController{
     @FXML
     private Button button0,button1,button2,button3,button4,button5,button6,
             button7,button8,button9,button10,button11,button12, button13, button14, button15;
-
     @FXML
-    private Line redLine;
+    private Line line0,line1,line2,line3,line4,line5,line6,line7,line8,line9,line10,line11,line12,line13,
+            line14,line15,line16,line17,line18,line19,line20,line21,line22,line23,line24,line25,line26,line27,
+            line28,line29,line30,line31,line32,line33,line34,line35,line36,line37,line38,line39;
+
+
+
 
     public String s = "Now Playing...";
 
@@ -51,33 +59,72 @@ public class gameController{
     public void endDot (MouseEvent event) {
     }
 
+
+    public void changeColor(MouseEvent event){
+        String eventlogger = event.getPickResult().getIntersectedNode().getId();
+        String selectedLineId = eventlogger.replaceAll("[\\D]", "");
+        int idx = Integer.parseInt(selectedLineId);
+        Line[] lines = {line0,line1,line2,line3,line4,line5,line6,line7,line8,line9,line10,line11,line12,line13,
+                line14,line15,line16,line17,line18,line19,line20,line21,line22,line23,line24,line25,line26,line27,
+                line28,line29,line30,line31,line32,line33,line34,line35,line36,line37,line38,line39};
+        for(int i=0; i<=lines.length; i = i+1){
+            if(lines[idx]==lines[i]){
+                if (Objects.equals(nowPlaying1.getText(), s) && !Objects.equals(lines[i].getStyle(),"-fx-stroke: #1e90ff")){
+                    lines[i].setStyle("-fx-stroke: #1e90ff;");
+                    lines[i].setOpacity(1.0);
+                    break;
+                }else if (Objects.equals(nowPlaying2.getText(), s) && !Objects.equals(lines[i].getStyle(),"-fx-stroke: #ff3916")){
+                    lines[i].setStyle("-fx-stroke: #ff3916;");
+                    lines[i].setOpacity(1.0);
+                    break;
+                }else{
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("cannot take this line");
+                    alert.setHeaderText("line already taken by the other player");
+                    alert.showAndWait();
+                    break;
+                }
+            }
+        }
+    }
+
     public void scoreUp(MouseEvent event){
             String eventlogger = event.getPickResult().getIntersectedNode().getId();
             String selectedButtonId = eventlogger.replaceAll("[\\D]", "");
             int idx = Integer.parseInt(selectedButtonId);
             Integer score1 = Integer.valueOf(scorePlayer1.getText());
             Integer score2 = Integer.valueOf(scorePlayer2.getText());
-            Button[] buttons = {button0, button1, button2, button3, button4, button5, button6,
-                    button7, button8, button9, button10,
-                    button11, button12, button13, button14, button15};
+        Button[] buttons = {button0, button1, button2, button3, button4, button5, button6,
+                button7, button8, button9, button10,
+                button11, button12, button13, button14, button15};
             for (int i = 0; i <= buttons.length; i = i + 1) {
-                if (buttons[idx] == buttons[i]) {
-                    if (nowPlaying1.getText() == s) {
-                        buttons[i].setText(String.valueOf(Player1Name.getText().charAt(0)).toUpperCase());
-                        buttons[i].setFont(Font.font(15));
-                        score1 = score1 + 1;
-                        scorePlayer1.setText(String.valueOf(score1));
-                    } else if (nowPlaying2.getText() == s) {
-                        buttons[i].setText(String.valueOf(Player2Name.getText().charAt(0)).toUpperCase());
-                        buttons[i].setFont(Font.font(15));
-                        score2 = score2 + 1;
-                        scorePlayer2.setText(String.valueOf(score2));
+                    if (buttons[idx] == buttons[i]) {
+                        if (Objects.equals(nowPlaying1.getText(), s) && !Objects.equals(buttons[i].getText(), String.valueOf(Player2Name.getText().charAt(0)).toUpperCase())) {
+                            buttons[i].setText(String.valueOf(Player1Name.getText().charAt(0)).toUpperCase());
+                            buttons[i].setFont(Font.font(15));
+                            score1 = score1 + 1;
+                            scorePlayer1.setText(String.valueOf(score1));
+                            buttons[i].setStyle("-fx-Background-color: #1e90ff;");
+                            break;
+                        } else if (Objects.equals(nowPlaying2.getText(), s) && !Objects.equals(buttons[i].getText(), String.valueOf(Player1Name.getText().charAt(0)).toUpperCase())) {
+                            buttons[i].setText(String.valueOf(Player2Name.getText().charAt(0)).toUpperCase());
+                            buttons[i].setFont(Font.font(15));
+                            score2 = score2 + 1;
+                            scorePlayer2.setText(String.valueOf(score2));
+                            buttons[i].setStyle("-fx-Background-color: #ff3916;");
+                            break;
+                        } else {
+                            Alert alert = new Alert(Alert.AlertType.ERROR);
+                            alert.setTitle("cannot take this box");
+                            alert.setHeaderText("box already taken by the other player");
+                            alert.showAndWait();
+                            break;
+                        }
                     }
-                    buttons[i].isDisabled();
-                    break;
-                }
             }
     }
+
+
 
     public void switchTurns (ActionEvent event) {
         if (nowPlaying1.getText()==s){

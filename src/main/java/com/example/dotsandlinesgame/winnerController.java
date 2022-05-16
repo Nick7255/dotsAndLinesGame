ecc0bc38d8d095133d25b0b95e5f0db0b6e3db7e
nick7255 <nikrass@cs.ihu.gr>
2022-05-17 02:18:53 +0300

    added restart button

--- src/main/java/com/example/dotsandlinesgame/winnerController.java
+++ src/main/java/com/example/dotsandlinesgame/winnerController.java
@@ -2,17 +2,24 @@ package com.example.dotsandlinesgame;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class winnerController {
 
     @FXML
     private Label winner;
     @FXML
-    private Button playAgain;
+    private Button playagain,newplayers;
     @FXML
     private AnchorPane anchornew;
 
@@ -24,6 +31,16 @@ public class winnerController {
     public void draw(){
         winner.setText("draw");
         anchornew.setStyle("-fx-Background-color: GREY");
+    }
+
+    public void backtostart(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("scene.fxml"));
+        Parent newroot = loader.load();
+        Controller restartcontroller = loader.getController();
+        Scene newscene = new Scene(newroot);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(newscene);
+        stage.show();
     }
 
 }
